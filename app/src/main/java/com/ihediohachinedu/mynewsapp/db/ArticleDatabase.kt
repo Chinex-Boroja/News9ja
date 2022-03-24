a0fcc8ca8fa0227920013831cfdf705da550dda3
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-24 11:01:57 +0100

    created article database

---
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/db/ArticleDatabase.kt
@@ -0,0 +1,33 @@
+package com.ihediohachinedu.mynewsapp.db
+
+import android.content.Context
+import androidx.room.*
+import com.ihediohachinedu.mynewsapp.model.Article
+
+@Database(
+    entities = [Article::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class ArticleDatabase : RoomDatabase() {
+
+    abstract fun getArticleDao(): ArticleDAO
+
+    companion object {
+        @Volatile
+        private var instance: ArticleDatabase? = null
+        private val LOCK = Any()
+
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDatabase::class.java,
+                "article_db.db"
+            ).build()
+    }
+}
