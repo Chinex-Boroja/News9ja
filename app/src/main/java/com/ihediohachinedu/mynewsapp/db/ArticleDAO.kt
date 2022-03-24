cd6d2780f42476ba3f087e7fb81763b944c32586
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-24 11:01:19 +0100

    created a DAO interface

---
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/db/ArticleDAO.kt
@@ -0,0 +1,18 @@
+package com.ihediohachinedu.mynewsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.ihediohachinedu.mynewsapp.model.Article
+
+@Dao
+interface ArticleDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
