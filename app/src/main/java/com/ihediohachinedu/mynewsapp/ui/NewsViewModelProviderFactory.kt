c1fd369bb6b855b2b6b84424394377174b482a78
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-24 10:49:51 +0100

    created NewsViewModelProviderFactory.kt

---
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/ui/NewsViewModelProviderFactory.kt
@@ -0,0 +1,13 @@
+package com.ihediohachinedu.mynewsapp.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ihediohachinedu.mynewsapp.repository.NewsRepository
+
+class NewsViewModelProviderFactory(val newsRepository: NewsRepository)
+    : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return NewsViewModel(newsRepository) as T
+    }
+}
