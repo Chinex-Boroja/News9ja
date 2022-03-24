14388aa51eb28ff47369094ae8671d3de2d198c6
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-24 10:52:06 +0100

    Created SavedNewsFragment.kt

--- app/src/main/java/com/ihediohachinedu/mynewsapp/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/ui/fragments/SavedNewsFragment.kt
@@ -1,7 +1,20 @@
 package com.ihediohachinedu.mynewsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.ihediohachinedu.mynewsapp.R
+import com.ihediohachinedu.mynewsapp.ui.NewsActivity
+import com.ihediohachinedu.mynewsapp.ui.NewsViewModel
 
 class SavedNewsFragment : Fragment(R.layout.saved_news_frag) {
+
+    lateinit var viewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = (activity as NewsActivity).viewModel
+
+    }
 }
