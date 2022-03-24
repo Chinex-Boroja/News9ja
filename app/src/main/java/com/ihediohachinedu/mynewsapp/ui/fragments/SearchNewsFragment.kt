a95ee54dbcb7c8156d519dc7856ddc254c64e465
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-24 10:51:29 +0100

    Created SearchNewsFragment.kt

--- app/src/main/java/com/ihediohachinedu/mynewsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/ui/fragments/SearchNewsFragment.kt
@@ -1,7 +1,20 @@
 package com.ihediohachinedu.mynewsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.ihediohachinedu.mynewsapp.R
+import com.ihediohachinedu.mynewsapp.ui.NewsActivity
+import com.ihediohachinedu.mynewsapp.ui.NewsViewModel
 
 class SearchNewsFragment : Fragment(R.layout.search_news_frag) {
+
+    lateinit var viewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = (activity as NewsActivity).viewModel
+
+    }
 }
