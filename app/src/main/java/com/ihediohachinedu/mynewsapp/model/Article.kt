39a58219f04d687c15c0c1847c56a9cfa57c8928
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-24 10:46:50 +0100

    added internet permission

--- app/src/main/java/com/ihediohachinedu/mynewsapp/model/Article.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/model/Article.kt
@@ -5,10 +5,11 @@ import androidx.room.PrimaryKey
 
 
 @Entity(
-    tableName = "article"
+    tableName = "articles"
 )
 data class Article(
     @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
