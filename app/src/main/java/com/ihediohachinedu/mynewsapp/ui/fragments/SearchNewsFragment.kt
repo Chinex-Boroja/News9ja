c0573d9e6337b87dbe7d0353fa2eb49f8af486d9
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-04-04 12:11:41 +0100

    added search functionalities

--- app/src/main/java/com/ihediohachinedu/mynewsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/ui/fragments/SearchNewsFragment.kt
@@ -1,20 +1,94 @@
 package com.ihediohachinedu.mynewsapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ihediohachinedu.mynewsapp.R
+import com.ihediohachinedu.mynewsapp.adapters.NewsAdapter
 import com.ihediohachinedu.mynewsapp.ui.NewsActivity
 import com.ihediohachinedu.mynewsapp.ui.NewsViewModel
+import com.ihediohachinedu.mynewsapp.utils.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import com.ihediohachinedu.mynewsapp.utils.Resource
+import kotlinx.android.synthetic.main.search_news_frag.*
+import kotlinx.android.synthetic.main.search_news_frag.paginationProgressBar
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchNewsFragment : Fragment(R.layout.search_news_frag) {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    val TAG = "SearchNewsFragment"
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_searchNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
 
+
+        var job: Job? = null
+        etSearch.addTextChangedListener { editable->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+                editable?.let {
+                    if (editable.toString().isNotEmpty()) {
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "Error Occurred: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
     }
 }
