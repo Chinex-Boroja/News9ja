d175d0cb4db453bc9a00d41ff76235ffe02d034d
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-04-04 12:07:11 +0100

    added functionalities for article

--- app/src/main/java/com/ihediohachinedu/mynewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/ui/fragments/ArticleFragment.kt
@@ -2,19 +2,34 @@ package com.ihediohachinedu.mynewsapp.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.ihediohachinedu.mynewsapp.R
 import com.ihediohachinedu.mynewsapp.ui.NewsActivity
 import com.ihediohachinedu.mynewsapp.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
     lateinit var viewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = (activity as NewsActivity).viewModel
+        val article = args.article
 
+        webView.apply {
+            webViewClient = WebViewClient()
+            article.url?.let { loadUrl(it) }
+        }
+        fab.setOnClickListener {
+            viewModel.saveArticle(article)
+            Snackbar.make(view, "Article saved successfully", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
