08a92e0bd417e94d543a47e6a60e6cd42a6fec86
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-04-04 12:04:09 +0100

    added annotations to data

--- app/src/main/java/com/ihediohachinedu/mynewsapp/model/NewsResponse.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/model/NewsResponse.kt
@@ -1,9 +1,13 @@
 package com.ihediohachinedu.mynewsapp.model
 
+import com.google.gson.annotations.SerializedName
 import com.ihediohachinedu.mynewsapp.model.Article
 
 data class NewsResponse(
-    val articles: List<Article>,
-    val status: String,
-    val totalResults: Int
+    @SerializedName("articles")
+    val articles: MutableList<Article>,
+    @SerializedName("status")
+    val status: String?,
+    @SerializedName("totalResults")
+    val totalResults: Int?
 )
