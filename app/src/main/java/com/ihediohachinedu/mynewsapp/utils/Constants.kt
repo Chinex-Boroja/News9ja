2a198c613395dfc6fa916f319355db1c4be13e99
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-04-04 12:10:46 +0100

    added search time delay

--- app/src/main/java/com/ihediohachinedu/mynewsapp/utils/Constants.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/utils/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "3148639a4eda4c1395d1c73eb7f19b00"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
