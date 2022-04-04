cc9302b222932ca41b8215317644c512bccf7e05
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-04-04 12:14:55 +0100

    refactored binding functionality

--- app/src/main/java/com/ihediohachinedu/mynewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/ui/NewsActivity.kt
@@ -2,13 +2,18 @@ package com.ihediohachinedu.mynewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.doOnPreDraw
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ihediohachinedu.mynewsapp.R
+import com.ihediohachinedu.mynewsapp.databinding.ActivityNewsBinding
 import com.ihediohachinedu.mynewsapp.db.ArticleDatabase
 import com.ihediohachinedu.mynewsapp.repository.NewsRepository
 import kotlinx.android.synthetic.main.activity_news.*
@@ -16,24 +21,34 @@ import kotlinx.android.synthetic.main.activity_news.*
 class NewsActivity : AppCompatActivity() {
 
     lateinit var viewModel: NewsViewModel
+    private lateinit var binding: ActivityNewsBinding
+    private val navController: NavController
+        get() = Navigation.findNavController(this, R.id.newsNavHostFragment)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+//        setContentView(R.layout.activity_news)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val newsRepository = NewsRepository(ArticleDatabase(this))
         val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
         viewModel = ViewModelProvider(this, viewModelProviderFactory).get((NewsViewModel::class.java))
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.news_nav_graph) as? NavHostFragment
-        val navController = navHostFragment?.navController
-
-        if (navController != null) {
-            bottomNavigationView.setupWithNavController(navController)
+        binding.root.doOnPreDraw {
+            binding.bottomNavigationView.setupWithNavController(navController)
         }
-       bottomNavigationView?.setupWithNavController(newsNavHostFragment.findNavController())
+
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+//        val navHostFragment = supportFragmentManager
+//            .findFragmentById(R.id.news_nav_graph) as? NavHostFragment
+//        val navController = navHostFragment?.navController
+//
+//        if (navController != null) {
+//            bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+//        }
+
+//       bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
 //        val navHostFragment = supportFragmentManager
 //            .findFragmentById(R.id.news_nav_graph) as NavHostFragment
 //        val navController = navHostFragment.navController
@@ -43,5 +58,13 @@ class NewsActivity : AppCompatActivity() {
 
 //        val navHostFragment = supportFragmentManager.findFragmentById(R.id.news_nav_graph) as NavHostFragment
 //        val navController = navHostFragment.navController
+//
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+//        val navController = findNavController(R.id.newsNavHostFragment)
+//        bottomNavigationView.setupWithNavController(navController)
     }
+
+//    override fun onStart() {
+//        super.onStart()
+//    }
 }
