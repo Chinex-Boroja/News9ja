df52e8b4fe089567defc4369edb9d28f406a87a8
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-04-04 12:06:18 +0100

    added CRUD functions

--- app/src/main/java/com/ihediohachinedu/mynewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/repository/NewsRepository.kt
@@ -2,6 +2,7 @@ package com.ihediohachinedu.mynewsapp.repository
 
 import com.ihediohachinedu.mynewsapp.api.RetrofitInstance
 import com.ihediohachinedu.mynewsapp.db.ArticleDatabase
+import com.ihediohachinedu.mynewsapp.model.Article
 
 class NewsRepository(
     val db: ArticleDatabase
@@ -9,4 +10,13 @@ class NewsRepository(
     //Call from our retrofit instance and pass the data
     suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
         RetrofitInstance.api.getBreakingNews(countryCode, pageNumber )
+
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
 }
