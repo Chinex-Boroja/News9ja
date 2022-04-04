f2435e29946c7d1aede979e5232ce494f815b18d
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-04-04 12:16:24 +0100

    added logic for handling CRUD responses

--- app/src/main/java/com/ihediohachinedu/mynewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/ui/NewsViewModel.kt
@@ -3,6 +3,7 @@ package com.ihediohachinedu.mynewsapp.ui
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ihediohachinedu.mynewsapp.model.Article
 import com.ihediohachinedu.mynewsapp.model.NewsResponse
 import com.ihediohachinedu.mynewsapp.repository.NewsRepository
 import com.ihediohachinedu.mynewsapp.utils.Resource
@@ -12,22 +13,50 @@ import retrofit2.Response
 class NewsViewModel (val newsRepository: NewsRepository) : ViewModel(){
 
     val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
-
     //for managing pagination
     var breakingNewsPage = 1
+    var breakingNewsResponse: NewsResponse? = null
+
+    val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    //for managing pagination
+    var searchNewsPage = 1
+    var searchNewsResponse: NewsResponse? = null
 
     init {
         getBreakingNews("ng")
     }
 
-    //A function that executes api call from the repository
+    //A function that executes api call from the repository and start a Coroutine
     fun getBreakingNews(countryCode: String) = viewModelScope.launch {
         breakingNews.postValue(Resource.Loading())
         val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
         breakingNews.postValue(handleBreakingNewsResponse(response))
     }
 
+    fun searchNews(searchQuery: String) = viewModelScope.launch {
+        searchNews.postValue(Resource.Loading())
+        val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+        searchNews.postValue(handleSearchNewsResponse(response))
+    }
+
     private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                breakingNewsPage++
+                if (breakingNewsResponse == null) {
+                    breakingNewsResponse = resultResponse
+                } else {
+                    val oldArticles = breakingNewsResponse?.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles?.addAll(newArticles)
+                }
+                return Resource.Success(breakingNewsResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
         if (response.isSuccessful) {
             response.body()?.let { resultResponse ->
                 return Resource.Success(resultResponse)
@@ -36,4 +65,12 @@ class NewsViewModel (val newsRepository: NewsRepository) : ViewModel(){
         return Resource.Error(response.message())
     }
 
+    fun saveArticle(article: Article) = viewModelScope.launch {
+        newsRepository.upsert(article)
+    }
+    fun getSavedNews() = newsRepository.getSavedNews()
+
+    fun deleteArticle(article: Article) = viewModelScope.launch {
+        newsRepository.deleteArticle(article )
+    }
 }
