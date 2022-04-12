eed04c795343a19ba20c4ac7d80c0888a573e823
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-04-12 03:24:15 +0100

    added query page size

--- app/src/main/java/com/ihediohachinedu/mynewsapp/utils/Constants.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/utils/Constants.kt
@@ -2,8 +2,9 @@ package com.ihediohachinedu.mynewsapp.utils
 
 class Constants {
     companion object{
-        const val API_KEY = "3148639a4eda4c1395d1c73eb7f19b00"
+        const val API_KEY = ""
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
