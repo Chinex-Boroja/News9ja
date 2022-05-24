ebcd3034c18563ef69286afafc8c7934c9004fcc
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-05-24 11:33:44 +0100

    modified viewmodel

--- app/src/main/java/com/ihediohachinedu/mynewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/ihediohachinedu/mynewsapp/ui/NewsActivity.kt
@@ -33,7 +33,7 @@ class NewsActivity : AppCompatActivity() {
 
         val newsRepository = NewsRepository(ArticleDatabase(this))
         val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
-        viewModel = ViewModelProvider(this, viewModelProviderFactory).get((NewsViewModel::class.java))
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
 
         binding.root.doOnPreDraw {
             binding.bottomNavigationView.setupWithNavController(navController)
